094c3ffd7be2cba7f70a14a763d21852fb83d6af
peterztan <pt072093@gmail.com>
2020-11-28 11:49:05 -0600

    added a custom implementation of a mergeSort algorithm

---
+++ CustomMergeSort.js
@@ -0,0 +1,32 @@
+const CustomMergeSort = inputArray => {
+    const length = inputArray.length;
+    if (length === 1) {
+        return inputArray;
+    }
+
+    const mid = Math.floor(length/2);
+    const leftArray = inputArray.slice(0, mid);
+    const rightArray = inputArray.slice(mid, length);
+
+    return CustomMerge(CustomMergeSort(leftArray), CustomMergeSort(rightArray));
+}
+
+const CustomMerge = (leftArray, rightArray) => {
+    const sortedArray = [];
+    while (leftArray.length > 0 && rightArray.length > 0) {
+        leftArray[0] < rightArray[0]
+        ? sortedArray.push(leftArray[0]) && leftArray.shift()
+        : sortedArray.push(rightArray[0]) && rightArray.shift();
+    }
+    return sortedArray.concat(leftArray).concat(rightArray);
+}
+
+const randomGenArray = [];
+while (randomGenArray.length < 100) {
+    const randomNumber = Math.floor(Math.random()*100);
+    randomGenArray.push(randomNumber);
+};
+console.log(randomGenArray);
+console.log(CustomMergeSort(randomGenArray));
+
+module.exports = CustomMergeSort;
