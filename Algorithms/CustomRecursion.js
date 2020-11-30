d00631013a9b7e7948f5658b1f08999afed251a8
peterztan <pt072093@gmail.com>
2020-11-29 22:22:06 -0600

    add Dijkstras algorithm conceptual description and update README

--- Algorithms/CustomRecursion.js
+++ Algorithms/CustomRecursion.js
@@ -1,3 +1,16 @@
+/**
+ * Recursion has two fundamental aspects: the base case and the recursive step.
+
+  When using iteration, we rely on a counting variable and a boolean condition.
+  For example, when iterating through the values in a list,
+  we would increment the counting variable until it exceeded the length of the dataset.
+
+  Recursive functions have a similar concept, which we call the base case.
+  The base case dictates whether the function will recurse, or call itself. Without a base case,
+  it’s the iterative equivalent to writing an infinite loop.
+ *  
+ */
+
 const recursiveFactorial = (n) => {
     if (n === 0) {
       //this is the base case: returns a value without self-call
